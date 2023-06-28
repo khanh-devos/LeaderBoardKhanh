f21fb2a9b1d3f97f283cc9a195c39c9b74d5d6fc
khanh-devos <dreamproperty.khanh@gmail.com>
2023-06-28 21:52:13 +0700

    create game ID by module createGameID()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import * as data from './modules/data.js';
 import * as scores from './modules/Score.js';
 import './style.css';
 
+data.createGameID();
 scores.getScores();
 document.querySelector('#scoresForm').addEventListener('submit', scores.addScore);
--- src/modules/data.js
+++ src/modules/data.js
@@ -1,21 +1,26 @@
+export const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+export const game = {id: null};
+
+
 export const setData = (scores) => {
   localStorage.setItem('scores', JSON.stringify(scores));
 };
 
-export const getData = () => (JSON.parse(localStorage.getItem('scores')) || [
-  {
-    index: 1,
-    name: 'k1',
-    score: 5,
-  },
-  {
-    index: 2,
-    name: 'k2',
-    score: 10,
-  },
-  {
-    index: 3,
-    name: 'k3',
-    score: 5,
-  },
-]);
+export const getData = () => {
+}
+
+export const createGameID = async () => {
+  const response = await fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'khanh_game'
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  
+  const {result } = await response.json();
+  game.id = result.split(' ').splice(3,1)[0];
+  console.log(game)
+}
